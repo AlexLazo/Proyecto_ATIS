9142b03a2080b69adb3868a5c5f7a66277673c58
Brian Alexander Fuentes Lazo <62920279+AlexLazo@users.noreply.github.com>
2022-10-27 00:57:46 -0600

    Dashboard

---
+++ dashboard.php
@@ -0,0 +1,110 @@
+<body>
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="dashboard.php">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+		</div>
+		
+		<div class="panel panel-container">
+			<div class="row">
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-teal panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-bed color-blue"></em>
+							<div class="text-muted">Habitaciones totales</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-blue panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-bookmark color-orange"></em>
+							<div class="text-muted">Reservaciones</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-orange panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-users color-teal"></em>
+							<div class="text-muted">Trabajadores</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-red panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-comments color-red"></em>
+							<div class="text-muted">Quejas</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr>
+
+			<div class="row">
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-teal panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-reorder color-red"></em>
+							<div class="text-muted">Habitaciones reservadas</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-blue panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-check-circle color-green"></em>
+							<div class="text-muted">Habitaciones disponibles</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-orange panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-check-square-o color-magg"></em>
+							<div class="text-muted">Checked-In</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+					<div class="panel panel-red panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-spinner color-blue"></em>
+							<div class="text-muted">Total de pagos pendientes</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr>
+
+			<div class="row">
+				<div class="col-xs-6 col-md-2 col-lg-2 no-padding">
+					
+				</div>
+
+				<div class="col-xs-6 col-md-4 col-lg-4 no-padding">
+					<div class="panel panel-red panel-widget border-right">
+						<div class="row no-padding"><em class="fa fa-xl fa-money color-red"></em>
+							<div class="text-muted">Total de Ganancias</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-4 col-lg-4 no-padding">
+					<div class="panel panel-orange panel-widget ">
+						<div class="row no-padding"><em class="fa fa-xl fa-credit-card color-purp"></em>
+							<div class="large">USD</div>
+							<div class="text-muted">Pago pendiente</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-6 col-md-2 col-lg-2 no-padding">
+					
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+
+		
+</body>
+</html>
