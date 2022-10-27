f2a25252fcac03c8b02a4ebb0226feb0473d827f
Brian Alexander Fuentes Lazo <62920279+AlexLazo@users.noreply.github.com>
2022-10-27 00:47:14 -0600

    bugs

--- ajax.php
+++ ajax.php
@@ -15,7 +15,7 @@ if (isset($_POST['login'])) {
         if (mysqli_num_rows($result) == 1) {
             $user = mysqli_fetch_assoc($result);
             $_SESSION['username'] = $user['username'];
-            $_SESSION['user_id'] = $user['id_Usuario'];
+            $_SESSION['id_Usuario'] = $user['id_Usuario'];
             header('Location:index.php?dashboard');
         } else {
             header('Location:login.php?loginE');
