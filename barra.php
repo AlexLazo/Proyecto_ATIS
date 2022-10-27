94d19e02c945b82aa3a451a4bb7c106199ab820a
Brian Alexander Fuentes Lazo <62920279+AlexLazo@users.noreply.github.com>
2022-10-27 00:34:58 -0600

    Bugs

--- barra.php
+++ barra.php
@@ -1,11 +1,10 @@
-
 <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
     <div class="profile-sidebar">
         <div class="profile-userpic">
             <img src="img/user.png" class="img-responsive" alt="">
         </div>
         <div class="profile-usertitle">
-            <div class="profile-usertitle-name"><?php echo $user['name'];?></div>
+            <div class="profile-usertitle-name"><?php echo $user['nombre'];?></div>
             <div class="profile-usertitle-status"><span class="indicator label-success"></span>Manager</div>
         </div>
         <div class="clear"></div>
